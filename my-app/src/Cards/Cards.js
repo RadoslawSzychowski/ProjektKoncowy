fa346bb183390a9476ced782c442c60e15aa2a64
Kanadelu <92264494+Kanadelu@users.noreply.github.com>
2024-02-18 15:34:19 +0100

    damage log

--- my-app/src/Cards/Cards.js
+++ my-app/src/Cards/Cards.js
@@ -9,16 +9,16 @@ class Card {
     }
 }
 
-const yaeMiko = new Card("Yae Miko", 100, 12, 3, 5);
-const raidenShogun = new Card("Raiden Shogun", 80, 17, 2, 5);
-const eula = new Card("Eula", 150, 7, 8, 4);
-const keqing = new Card("Keqing", 200, 4, 16, 4);
-const ganyu = new Card("Ganyu", 110,8,8,4 );
-const alhaitham = new Card("Alhaitham", 130, 5, 9, 3);
-const zhongli = new Card("Zhongli", 86, 15, 3, 4);
-const dehya = new Card("Dehya", 50, 22, 1, 4);
-const yelan = new Card("Yelan", 90, 13, 6, 5);
-const chongyun = new Card("Chongyun", 75, 9, 9, 4);
+const yaeMiko = new Card("Yae Miko", 100, 12, 3, 6);
+const raidenShogun = new Card("Raiden Shogun", 80, 17, 2, 6);
+const eula = new Card("Eula", 150, 3, 4, 4);
+const keqing = new Card("Keqing", 100, 4, 3, 4);
+const ganyu = new Card("Ganyu", 110,8,3,5 );
+const alhaitham = new Card("Alhaitham", 130, 5, 1, 3);
+const zhongli = new Card("Zhongli", 86, 15, 3, 5);
+const dehya = new Card("Dehya", 50, 22, 0, 4);
+const yelan = new Card("Yelan", 90, 9, 6, 4);
+const chongyun = new Card("Chongyun", 75, 9, 2, 4);
 
 
 export const deck = [yaeMiko, raidenShogun, eula, keqing, ganyu, alhaitham, zhongli, dehya, yelan, chongyun];
--- my-app/src/GamePage/Game.jsx
+++ my-app/src/GamePage/Game.jsx
@@ -2,35 +2,40 @@ import React, { useState, useEffect } from 'react';
 import Deck from '../Cards/CardComponents/Deck';
 import { deck } from '../Cards/Cards';
 import { shuffleAndDeal } from '../Cards/CardComponents/ShuffleAndDeal';
-import './Game.scss'
-import {startGame} from '../Cards/CardComponents/StartGameButton'
-import {pauseGame} from '../Cards/CardComponents/PauseGameButton'
-import {newGame} from '../Cards/CardComponents/NewGameButton'
-
+import './Game.scss';
+import { startGame } from '../Cards/CardComponents/StartGameButton';
+import { pauseGame } from '../Cards/CardComponents/PauseGameButton';
+import { newGame } from '../Cards/CardComponents/NewGameButton';
 
 const Game = () => {
   const [playerCards, setPlayerCards] = useState([]);
   const [computerCards, setComputerCards] = useState([]);
   const [attackMode, setAttackMode] = useState({ isActive: false, attackingCard: null });
-
-
+  const [logMessage, setLogMessage] = useState("");
+  const [isLogVisible, setIsLogVisible] = useState(false);
 
   useEffect(() => {
     const { playerCards, computerCards } = shuffleAndDeal(deck);
     setPlayerCards(playerCards);
     setComputerCards(computerCards);
-    const damagedPlayerCards = playerCards.map(card => { // zmniejszenie hp kart tempo
-      return { ...card, hp: card.hp - Math.floor(Math.random() * 90) }; // zmniejszenie hp kart tempo
-    });
-
-    setPlayerCards(damagedPlayerCards);
   }, []);
+
+  useEffect(() => {
+    if (isLogVisible) {
+      const timer = setTimeout(() => {
+        setIsLogVisible(false);
+      }, 2000);
+      return () => clearTimeout(timer);
+    }
+  }, [isLogVisible]);
 
   const handleHeal = (cardToHeal, healValue) => {
     setPlayerCards(currentCards => {
       return currentCards.map(card => {
         if (card.name === cardToHeal.name) {
           const newHp = Math.min(card.maxHp, card.hp + healValue);
+          setLogMessage(`${cardToHeal.name} healed ${healValue} HP`);
+          setIsLogVisible(true);
           return { ...card, hp: newHp };
         }
         return card;
@@ -42,11 +47,13 @@ const Game = () => {
     if (attackMode.isActive && attackMode.attackingCard) {
       if (attackMode.attackingCard.name !== selectedCard.name) {
         const damage = Math.max(attackMode.attackingCard.attack - selectedCard.defense, 5);
+        setLogMessage(`${attackMode.attackingCard.name} dealt ${damage} damage to ${selectedCard.name}`);
+        setIsLogVisible(true);
 
         const updateCards = (cards, cardToDamage) => {
           return cards.map(card => {
             if (card.name === cardToDamage.name) {
-              return { ...card, hp: Math.max(card.hp - damage, 0), recentDamage: damage };
+              return { ...card, hp: Math.max(card.hp - damage, 0) };
             }
             return card;
           });
@@ -65,24 +72,23 @@ const Game = () => {
     }
   };
 
-  
-  return (<>
-    <div className="game-background-container">
-      <video src="images/gameBg.mp4" autoPlay muted loop className="game-background-video" />
-      <div className="game-overlay"></div>
-    </div>
-    <div className='game-container'>
-      <h2>Computer's Cards:</h2>
-      <Deck deck={computerCards} onAttack={handleAttack} attackMode={attackMode} />
-      <h2>Player's Cards:</h2>
-      <Deck deck={playerCards} onHeal={handleHeal} onAttack={handleAttack} attackMode={attackMode} />
-      <div className="game-controls">
-        <button onClick={startGame} className="game-button start-game">Start Game</button>
-        <button onClick={pauseGame} className="game-button pause-game">Pause</button>
-        <button onClick={newGame} className="game-button new-game">New Game</button>
+  return (
+    <>
+      <div className="game-background-container">
+        <video src="images/gameBg.mp4" autoPlay muted loop className="game-background-video" />
+        <div className="game-overlay"></div>
       </div>
-    </div>
-  </>
+      <div className='game-container'>
+        <Deck deck={computerCards} onAttack={handleAttack} attackMode={attackMode} />
+        <p className={`log ${isLogVisible ? 'active' : ''}`}>{logMessage}</p>
+        <Deck deck={playerCards} onHeal={handleHeal} onAttack={handleAttack} attackMode={attackMode} />
+        <div className="game-controls">
+          <button onClick={startGame} className="game-button start-game">Start Game</button>
+          <button onClick={pauseGame} className="game-button pause-game">Pause</button>
+          <button onClick={newGame} className="game-button new-game">New Game</button>
+        </div>
+      </div>
+    </>
   );
 };
 
